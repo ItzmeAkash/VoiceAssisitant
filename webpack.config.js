48685d41b180bff7632803390b870f20e41bbee8
Akash <akashpsnikku@gmail.com>
2024-05-18 18:43:06 +0530

    removed unnessary files

--- webpack.config.js
+++
@@ -1,8 +0,0 @@
-// webpack.config.js
-const fallbackConfig = require('./webpack.fallbacks.js');
-
-module.exports = {
-  // Other webpack configurations...
-  ...fallbackConfig,
-  // Other webpack configurations...
-};
